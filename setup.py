7814e4fac95772113c6060f57e403838dc5a6933
kamal shadi <kshadi@cisco.com>
2017-06-16 04:45:28 -0400

    tags

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='ipclass',
+setup(name='mania',
       version='0.1.0',
       description='MANIA anatomical inference',
       url='',
       author='Kamal Shadi',
       author_email='kamal.shadi85@gmail.com',
       license='MIT',
+      download_url = 'https://github.com/kamalshadi/mania/archive/0.1.tar.gz',
       packages=['mania'],
       zip_safe=False)
