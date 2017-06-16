dfeaf5190b9007a3159b14795aeae2aa28ed2e83
kamal shadi <kshadi@cisco.com>
2017-06-16 05:07:37 -0400

    pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pymania',
-      version='0.1',
+      version='0.1.2',
       description='MANIA anatomical inference',
       url='https://github.com/kamalshadi/mania/',
       author='Kamal Shadi',
       author_email='kamal.shadi85@gmail.com',
       license='MIT',
-      download_url = 'https://github.com/kamalshadi/mania/archive/0.1.tar.gz',
+      download_url = 'https://github.com/kamalshadi/mania/archive/0.1.2.tar.gz',
       packages=['mania'],
       zip_safe=False)
